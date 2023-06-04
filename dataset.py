9c425bf679d6b0686efa8899ad38f81860f26a25
Dasha Sapog <d.sapogmail@yandex.ru>
2023-06-05 02:37:31 +0800

    commit error

--- dataset.py
+++ dataset.py
@@ -6,9 +6,7 @@ from PIL import Image
 from random import randint
 from torch.utils.data import Dataset
 from tqdm.notebook import tqdm
-
-
-uni2sym_df = pd.read_csv('./data/unicode_translation.csv')
+uni2sym_df = pd.read_csv('./unicode_translation.csv')
 forgotten = [{'Unicode': 'U+770C', 'char': '県'},
              {'Unicode': 'U+4FA1', 'char': '価'},
              {'Unicode': 'U+7A83', 'char': '窃'},
@@ -18,14 +16,11 @@ forgotten = [{'Unicode': 'U+770C', 'char': '県'},
 uni2sym_df = pd.concat([uni2sym_df, pd.DataFrame.from_records(forgotten)])
 uni2class = dict(zip(uni2sym_df.Unicode, uni2sym_df.index.tolist()))
 class2sym = dict(zip(uni2sym_df.index.tolist(), uni2sym_df.char))
-
-
-def load_data(split, path):
+def load_data(split):
     assert split in ('train', 'test')
     csv_split = 'train' if split == 'train' else 'sample_submission'
-    df = pd.read_csv(f'{path}/{csv_split}.csv', keep_default_na=False)
-    image_path = Path(f'{path}/{split}')
-
+    df = pd.read_csv(f'./{csv_split}.csv', keep_default_na=False)
+    image_path = Path(f'./data/{split}')
     images = []
     for id, row in tqdm(df.iterrows(), total=len(df)):
         id_name = row['image_id'] + '.jpg'
@@ -48,11 +43,11 @@ def load_data(split, path):
         image['labels'] = np.array(image['labels'])
         images.append(image)
     return images
-
-
 def ourCrop(image, bboxes, labels, w, h, n_w, n_h, thresh=0.33, max_labels=50):
     x0, y0 = randint(0, w - n_w), randint(0, h - n_h)
     x1, y1 = x0 + n_w, y0 + n_h
+    new_bboxes = torch.full((max_labels, 4), -1)
+    new_labels = torch.full((max_labels, 1), -1)
     new_bboxes = torch.full((max_labels, 4), 0)
     new_labels = torch.full((max_labels, 1), 0)
     fill_i = 0
@@ -73,24 +68,20 @@ def ourCrop(image, bboxes, labels, w, h, n_w, n_h, thresh=0.33, max_labels=50):
     else:
         raise ValueError('unknown image type in crop')
     return new_image, np.array(new_bboxes), np.array(new_labels)
-
-
 class DetectionDataset(Dataset):
     def __init__(self, images, max_size=None, crop_size=(1024, 1024), transforms=None, threshold=0.5):
         if isinstance(crop_size, int):
             crop_size = (crop_size, crop_size)
         if isinstance(images, str):
-            self.images = load_data(images, 'data')
+            self.images = load_data(images)
         else:
             self.images = images
         self.max_size = max_size
         self.crop_size = crop_size
         self.transforms = transforms
         self.threshold = threshold
-
     def __len__(self):
         return len(self.images)
-
     def __getitem__(self, idx):
         image = Image.open(self.images[idx]['file'])
         bboxes = self.images[idx]['bboxes']  # pixels: (x, y, w, h)
