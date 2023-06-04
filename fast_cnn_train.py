a2406075af1f6f70d57ef7c363c52fa376654281
Dasha Sapog <d.sapogmail@yandex.ru>
2023-06-04 23:39:50 +0800

    Add empty fcrnn train file

---
+++ fast_cnn_train.py
@@ -0,0 +1,14 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+import torch
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+import torch.utils.data
+
+from PIL import Image, ImageFile
+
+from tqdm import tqdm
