78d810a455e50d1ab301ac8ff392a8d4f808cc9a
Dasha Sapog <d.sapogmail@yandex.ru>
2023-06-05 18:27:34 +0800

    evaluation

--- utils.py
+++ utils.py
@@ -5,8 +5,9 @@ from dataset import DetectionDataset, class2sym
 from PIL import Image, ImageDraw, ImageFont
 
 
-def draw_bboxes(image, bboxes, labels, order='xywh', mode='pixels'):
-    if isinstance(image, np.ndarray):
+def draw_bboxes(image, bboxes, labels, order='xywh', mode='pixels', draw_class=True,
+                borderwidth=3):
+    if isinstance(image, np.ndarray) or isinstance(image, torch.Tensor):
         image = Image.fromarray(image)
     elif isinstance(image, torch.Tensor):
         image = np.moveaxis(image.numpy() * 255, 0, -1)
@@ -26,10 +27,11 @@ def draw_bboxes(image, bboxes, labels, order='xywh', mode='pixels'):
             a0, b0, a1, b1 = x0, y0, x0 + x1, y0 + y1
         else:
             raise ValueError('unknown mode in draw bboxes')
-        draw.rectangle([a0, b0, a1, b1], outline=rgb, width=3)
-        draw.rectangle((x0, y0 - 14, x0 + 18, y0 + 4), fill=(255, 255, 255))
-        fnt = ImageFont.truetype("./Arial Unicode MS.TTF", 14)
-        draw.text((x0 + 2, y0 - 12), class2sym[class_i], fill=(0, 0, 0), font=fnt)
+        draw.rectangle([a0, b0, a1, b1], outline=rgb, width=borderwidth)
+        if draw_class:
+            draw.rectangle((x0, y0 - 14, x0 + 18, y0 + 4), fill=(255, 255, 255))
+            fnt = ImageFont.truetype("./Arial Unicode MS.TTF", 14)
+            draw.text((x0 + 2, y0 - 12), class2sym[class_i], fill=(0, 0, 0), font=fnt)
     return image
 
 
