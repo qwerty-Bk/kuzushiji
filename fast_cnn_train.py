c97ee9752ee192068013d66c6f51b808e0eb06c8
Dasha Sapog <d.sapogmail@yandex.ru>
2023-06-05 10:24:13 +0800

    add loss logging

--- fast_cnn_train.py
+++ fast_cnn_train.py
@@ -1,6 +1,8 @@
 import os
 from datetime import datetime
 
+import pickle
+
 import numpy as np
 import pandas as pd
 
@@ -19,72 +21,77 @@ from tqdm import tqdm
 from dataset import DetectionDataset, class2sym
 
 DEVICE =  'cuda' if torch.cuda.is_available() else 'cpu'
+#DEVICE = 'cpu'
 
-LR = 0.005
+LR = 0.0005
 MOMENTUM = 0.9
 WD = 0.0005
 GAMMA = 0.1
-N_EPOCH = 100
+N_EPOCH = 50
 
 
 def create_model(pretrained):
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=pretrained)
     in_features = model.roi_heads.box_predictor.cls_score.in_features
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, 2)
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, len(class2sym))
 
     return model
 
 
 def build_target(image, bboxes, labels):
-    transform = T.Compose([T.ColorJitter(0.3, 0.5, 0.5, 0.1),
-                            T.ToTensor(),
-                            T.Normalize(0, 255)])
-
-    images = transform(image).to(DEVICE)
+    images = image.to(DEVICE)
+    labels = labels.to(DEVICE)
+    
+    bboxes = bboxes.type(torch.FloatTensor).to(DEVICE)
 
     true_labels = torch.ones(labels.size()).to(DEVICE)
     
-    images = images.reshape(1, 3, 1024, 1024)
-    bboxes = bboxes.reshape(1, bboxes.shape[0], -1)
-
-    bboxes[:,:,0] = bboxes[:,:,0] - bboxes[:,:,3] 
-    bboxes[:,:,3] = bboxes[:,:,0] + bboxes[:,:,3]
-    bboxes[:,:,1] = bboxes[:,:,1] + bboxes[:,:,2]
-    temp = torch.zeros(bboxes.shape[0], bboxes.shape[1], 1)
-    #temp = bboxes[:,:,1]
-    #bboxes[:,:,1] = bboxes[:,:,2]
-    #bboxes[:,:,2] = temp
+    #images = images.reshape(1, 3, 512, 512)
+    #bboxes = bboxes.reshape(1, bboxes.shape[0], -1)
+    #labels = labels.reshape(1,labels.shape[0], -1)
+    
+    bboxes[:,:,2] = bboxes[:,:,0] + bboxes[:,:,2]
+    bboxes[:,:,3] = bboxes[:,:,1] + bboxes[:,:,3]
+    
+    
     bboxes[bboxes < 0] = 0.
+    mask_bad = torch.zeros(bboxes.shape[0], bboxes.shape[1], bboxes.shape[2]).to(DEVICE)
+    mask_bad[:,:,2] = 1
+    mask_bad[:,:,3] = 1
 
+    bboxes += mask_bad * (bboxes < 0.001) * 0.001
     targets = []
 
     for i in range(bboxes.shape[0]):
         targets.append({'boxes': bboxes[i], 
-                          'labels': true_labels[i]})
-
-    #print(images.shape)
+                          'labels': labels[i].reshape(-1)})
+    #print(targets)
     return images, targets
     
 
 def train_one_epoch(model, optimizer, scheduler, dataloader):
     running_losses = []
     print(len(dataloader))
-    for image, bboxes, labels in tqdm(iter(dataloader)):
-        try:
-            images, targets = build_target(image, torch.Tensor(bboxes).to(DEVICE), torch.LongTensor(labels))
+    #for image, bboxes, labels in tqdm(iter(dataloader)):
+    pcs = tqdm(enumerate(dataloader))
+    for batch_idx, (image, bboxes, labels) in pcs:
         
-            predict_loss = model(images, targets)
+        images, targets = build_target(image, torch.Tensor(bboxes).to(DEVICE), torch.LongTensor(labels))
+        
+        predict_loss = model(images, targets)
 
-            loss = sum(loss for loss in predict_loss.values())
-            running_losses.append(loss.item())
+        loss = sum(loss for loss in predict_loss.values())
+        #print(loss)
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            print("Good")
-        except:
-            #print('Cropped')
-            pass
+        pcs.set_description(f"Loss {loss}")
+
+        running_losses.append(loss.item())
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if (batch_idx % 40) == 0:
+            print(loss)
 
     return running_losses
 
@@ -92,8 +99,15 @@ def train_one_epoch(model, optimizer, scheduler, dataloader):
 def train(model, optimizer, scheduler, dataloader, n_epoch):
     running_losses = []
     for i in range(n_epoch):
+        print('Starting epoch', i)
         epoch_losses = train_one_epoch(model, optimizer, scheduler, dataloader)
         running_losses.extend(epoch_losses)
+
+        file_name = 'checkpoints/fast-crnn/' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '_epoch_' + str(i) + '.path'
+        os.makedirs("checkpoints", exist_ok=True)
+        os.makedirs("checkpoints/fast-crnn", exist_ok=True)
+        torch.save(model, file_name)
+
     return running_losses
 
 
@@ -116,16 +130,30 @@ if __name__ == '__main__':
 
     print(DEVICE)
 
-    dataloader = DetectionDataset('train', max_size=2048, transforms=T.Compose([T.ColorJitter(0.3, 0.5, 0.5, 0.1)]))
+    transform = T.Compose([T.ColorJitter(0.3, 0.5, 0.5, 0.1),
+                            T.ToTensor(),
+                            T.Normalize(0, 255)])
 
-    model = create_model(None)
+    #dataloader = DetectionDataset('train', max_size=2048, crop_size=(512, 512), transforms=transform)
+    dataset = DetectionDataset('train', max_size=2048, crop_size=(512, 512), transforms=transform, max_labels=15)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=True, num_workers=2)
+
+    model = create_model(None).to(DEVICE)
 
     optimizer = torch.optim.SGD(model.parameters(), lr=LR, momentum=MOMENTUM, weight_decay=WD)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=GAMMA)
 
-    train(model, optimizer, scheduler, dataloader, N_EPOCH)
+    final_losses = train(model, optimizer, scheduler, dataloader, N_EPOCH)
+
+
+    with open("losses", "wb") as fp:   #Pickling
+        pickle.dump(final_losses, fp)
+
+    with open("file_losses.txt", 'w') as f:
+        for s in final_losses:
+            f.write(str(s) + '\n')
     
-    file_name = 'checkpoints/fast-crnn/' + datetime.now().strftime('%Y-%m-%d-%H:%M:%S') + '.path'
+    file_name = 'checkpoints/fast-crnn/' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.path'
     os.makedirs("checkpoints", exist_ok=True)
     os.makedirs("checkpoints/fast-crnn", exist_ok=True)
     torch.save(model, file_name)
